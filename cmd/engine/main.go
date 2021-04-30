aa26b714789f762a54a06e7002a7ebc20a8a455e
scottcagno <scottiecagno@gmail.com>
2021-04-30 18:25:52 -0400

    working on some basic file handling stuff

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"github.com/scottcagno/net-tools/pkg/engine"
@@ -9,6 +10,8 @@ import (
 )
 
 // https://play.golang.org/p/gaWCeLMiXZG
+// https://play.golang.org/p/i7Rkw_fH1p2 <-- updated one
+// https://play.golang.org/p/adqdT4lSuaZ <-- most up to date one
 
 func main() {
 	e := engine.Open("cmd/engine/test/data.txt")
@@ -38,6 +41,13 @@ func pad(d []byte, n int) []byte {
 	return d
 }
 
+// blocks returns how many blocks n bytes would occupy
+// after it has been padded and aligned properly
+func blocks(n int) int {
+	b := align(n+2, blocksize)
+	return b / blocksize
+}
+
 func insert(s []byte, k int, vs ...byte) []byte {
 	if n := len(s) + len(vs); n <= cap(s) {
 		s2 := s[:n]
@@ -57,8 +67,30 @@ func record(d []byte) []byte {
 	// pad out the record to the specified blocksize
 	d = insert(d, 0, []byte{0, 0}...)
 	z := align(len(d), blocksize) - len(d)
+	b := blocks(len(d))
 	d = pad(d, z)
-	blocks := len(d) / blocksize
-	binary.PutUvarint(d[0:2], uint64(blocks))
+	binary.PutUvarint(d[0:2], uint64(b))
 	return d
+}
+
+func record2(d []byte) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	blk := blocks(len(d))
+	tmp := make([]byte, 2, 2)
+	binary.LittleEndian.PutUint16(tmp, uint16(blk))
+	_, err := buf.Write(tmp)
+	if err != nil {
+		return nil, err
+	}
+	tmp = nil
+	_, err = buf.Write(d)
+	if err != nil {
+		return nil, err
+	}
+	remaining := blk*blocksize - buf.Len()
+	_, err = buf.Write(make([]byte, remaining))
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
